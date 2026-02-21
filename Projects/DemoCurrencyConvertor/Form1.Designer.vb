44085e2a13a9c8ced738735815a6cc8fc25902e3
BB <lordgiver@hotmail.com>
2026-02-21 10:27:54 +0000

    Demo VB.NET currency converter program

---
+++ Projects/DemoCurrencyConvertor/Form1.Designer.vb
@@ -0,0 +1,158 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtDollar = New System.Windows.Forms.TextBox()
+        Me.btnClose = New System.Windows.Forms.Button()
+        Me.btnConvert = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblCedi = New System.Windows.Forms.Label()
+        Me.lblNira = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.txtDollar)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(428, 105)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Convert from:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(15, 37)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(223, 29)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "American Dollars ($): "
+        '
+        'txtDollar
+        '
+        Me.txtDollar.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtDollar.Location = New System.Drawing.Point(244, 37)
+        Me.txtDollar.Name = "txtDollar"
+        Me.txtDollar.Size = New System.Drawing.Size(167, 37)
+        Me.txtDollar.TabIndex = 1
+        '
+        'btnClose
+        '
+        Me.btnClose.Location = New System.Drawing.Point(246, 281)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(177, 55)
+        Me.btnClose.TabIndex = 1
+        Me.btnClose.Text = "Close"
+        Me.btnClose.UseVisualStyleBackColor = True
+        '
+        'btnConvert
+        '
+        Me.btnConvert.Location = New System.Drawing.Point(32, 281)
+        Me.btnConvert.Name = "btnConvert"
+        Me.btnConvert.Size = New System.Drawing.Size(177, 54)
+        Me.btnConvert.TabIndex = 2
+        Me.btnConvert.Text = "Convert"
+        Me.btnConvert.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(27, 145)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(218, 29)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Ghanaian Cedi (GH): "
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(27, 207)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(185, 29)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Nigerian Nira (N):"
+        '
+        'lblCedi
+        '
+        Me.lblCedi.AutoSize = True
+        Me.lblCedi.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblCedi.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCedi.Location = New System.Drawing.Point(251, 145)
+        Me.lblCedi.Name = "lblCedi"
+        Me.lblCedi.Size = New System.Drawing.Size(80, 31)
+        Me.lblCedi.TabIndex = 5
+        Me.lblCedi.Text = "Label3"
+        Me.lblCedi.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblNira
+        '
+        Me.lblNira.AutoSize = True
+        Me.lblNira.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblNira.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblNira.Location = New System.Drawing.Point(247, 214)
+        Me.lblNira.Name = "lblNira"
+        Me.lblNira.Size = New System.Drawing.Size(80, 31)
+        Me.lblNira.TabIndex = 6
+        Me.lblNira.Text = "Label4"
+        Me.lblNira.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(458, 349)
+        Me.Controls.Add(Me.lblNira)
+        Me.Controls.Add(Me.lblCedi)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.btnConvert)
+        Me.Controls.Add(Me.btnClose)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtDollar As TextBox
+    Friend WithEvents btnClose As Button
+    Friend WithEvents btnConvert As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lblCedi As Label
+    Friend WithEvents lblNira As Label
+End Class
---
+++ Projects/DemoCurrencyConvertor/Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnConvert_Click(sender As Object, e As EventArgs) Handles btnConvert.Click
+        Dim dollar As Double
+        Dim cedi As Double
+        Dim nira As Double
+        Dim ghRate As Double = 11.0
+        Dim nigRate As Double = 1500.0
+
+        dollar = CDbl(txtDollar.Text)
+        cedi = dollar * ghRate
+        nira = dollar * nigRate
+
+        lblCedi.Text = cedi
+        lblNira.Text = nira
+    End Sub
+End Class
